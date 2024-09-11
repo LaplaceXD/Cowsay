649dbec119a1d54d4e85ee266e65dccc8bbcd918
Jonh Alexis Buot <ja.buotph@gmail.com>
2024-09-11 23:49:48 +0800

    feat(main): created the console app to interface the user with the cowsay class process wrapper

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel;
+
+try
+{
+    using Cowsay cowsay = new();
+
+    Console.Write("-> Tell me what you want to say: ");
+    string? message = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(message))
+    {
+        Console.WriteLine("error: message cannot be empty.");
+        return;
+    }
+
+    string cowsayResponse = cowsay.Say(message);
+    Console.WriteLine(cowsayResponse);
+}
+catch (Win32Exception)
+{
+    Console.WriteLine("error: cowsay is not installed on this system.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"error: {ex.Message}");
+}
