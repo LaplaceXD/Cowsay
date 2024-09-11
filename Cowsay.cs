24e6e2eaf0f962c26ad1c6e680a489128ae9d9d9
Jonh Alexis Buot <ja.buotph@gmail.com>
2024-09-12 00:58:31 +0800

    chore(docs): added documentation to the methods and fields of the Cowsay class

--- Cowsay.cs
+++ Cowsay.cs
@@ -1,11 +1,27 @@
 using System.Diagnostics;
 
+/// <summary>
+/// Cowsay is a program that generates ASCII pictures of a cow with a message.
+/// </summary>
 class Cowsay : IDisposable
 {
     private Process _process;
+
+    /// <summary>
+    /// Event handler for receiving data from Cowsay.
+    /// </summary>
     public event DataReceivedEventHandler? OutputDataReceived;
+
+    /// <summary>
+    /// Event handler for receiving error data from Cowsay.
+    /// </summary>
     public event DataReceivedEventHandler? ErrorDataReceived;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Cowsay"/> class.
+    /// </summary>
+    ///
+    /// <exception cref="Win32Exception">The Cowsay program is not installed.</exception>
     public Cowsay()
     {
         ProcessStartInfo startInfo = new()
@@ -28,6 +44,11 @@ class Cowsay : IDisposable
         _process.Start();
     }
 
+    /// <summary>
+    /// Say a message with Cowsay, which generates an ASCII picture of a cow with the message.
+    /// </summary>
+    ///
+    /// <param name="message">The message to say.</param>
     public void Say(string message)
     {
         _process.StandardInput.WriteLine(message);
@@ -38,6 +59,9 @@ class Cowsay : IDisposable
         _process.WaitForExit();
     }
 
+    /// <summary>
+    /// Releases all resources used by the <see cref="Cowsay"/>.
+    /// </summary>
     public void Dispose()
     {
         _process.Dispose();
